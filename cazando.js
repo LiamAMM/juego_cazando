85e6ad44033b49fb77f53b8bb75b81bdd446a858
LiamAMM <arielmorales142578@gmail.com>
2026-09-10 20:49:14 -0500

    agregar graficarComida, iniciarJuego, variabless y constantes de posicion

--- cazando.js
+++ cazando.js
@@ -1,7 +1,27 @@
 let canvas=document.getElementById ("areaJuego");
 let ctx=canvas.getContext("2d");
 
+let gatoX=canvas.width
+let gatoY=canvas.height
+let comidaX=500-50
+let comidaY=500-50
+
+const ALTO_GATO=60
+const ANCHO_GATO=40
+const ALTO_COMIDA=50
+const ANCHO_COMIDA=50
+
 function graficarGato(){
     ctx.fillStyle="purple";
-    ctx.fillRect(0,(canvas.height-50)/2,canvas.width,50);
+    ctx.fillRect((gatoX-ANCHO_GATO)/2,(gatoY-ALTO_GATO)/2,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle="green";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
